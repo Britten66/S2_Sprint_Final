d383650d796fcf3d0e5d5390f4b32121d5b83319
Chris <Britten63@hotmail.com>
2025-12-09 16:30:36 -0400

    Landing Page Created

---
+++ steem-app/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>Steam Clone</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
