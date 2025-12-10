32cb7ae6accfb7701d6d5398291beb3517f14736
Chris <Britten63@hotmail.com>
2025-12-10 09:27:57 -0400

    landing page - linked

--- steem-app/src/App.jsx
+++ steem-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import LandingPage from "./pages/Landing-Page/landing.jsx";
 import LoginPage from "./pages/Log-in-Page/login.jsx";
+import StorePage from "./pages/Store-Page/store.jsx";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/store" element={<StorePage />} />
       </Routes>
     </Router>
   );
