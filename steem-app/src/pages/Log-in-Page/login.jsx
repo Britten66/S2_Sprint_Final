dceb129297f3be326651b6c2e28b2a3bc44fa6ec
Chris <Britten63@hotmail.com>
2025-12-18 22:05:23 -0400

    Fix Footer.css casing

--- steem-app/src/pages/Log-in-Page/login.jsx
+++ steem-app/src/pages/Log-in-Page/login.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 const LoginPage = () => {
   const [userEmail, setUserEmail] = useState("");
